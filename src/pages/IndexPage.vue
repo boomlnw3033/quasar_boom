6d75e6570fa4392a55484357e7586e4e68d3f07f
Chayanat Chunmuangphan <boom3033@hotmail.com>
2023-12-08 11:07:27 +0700

    Chayanat 查亚纳·查穆旺潘 6404101309

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,8 +9,8 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="你的名字*"
+        hint="名和姓"
         lazy-rules
         :rules="[ val => val && val.length > 0 || 'Please type something']"
       />
