b5b35ef189963183f32950fddc2911f06d9d2a6d
Chayanat Chunmuangphan <boom3033@hotmail.com>
2023-12-08 10:29:32 +0700

    quasar_test_INPUT_ไทย

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -9,10 +9,10 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อจริง *"
+        hint="ชื่อ + นามสกุล"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'กรุณาพิมพ์ชื่อจริง']"
       />
       <q-input
         filled
